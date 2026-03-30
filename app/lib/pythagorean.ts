06a1bbd19a8c7398d6562809f8f59b3a37da43ed
Hieu To <34306914+hieudeptrai09@users.noreply.github.com>
2026-03-30 08:04:58 +0700

    Fix return statement in divisor function

--- app/lib/pythagorean.ts
+++ app/lib/pythagorean.ts
@@ -35,7 +35,7 @@ function getDivisors(n: number): number[] {
   for(let i = 1; i <= n; i++) {
     if(n % i === 0) result.push(i)
   }
-  return result[]
+  return result;
 }
 
 function powerAndMod(base: number, exp: number, mod: number): number {
